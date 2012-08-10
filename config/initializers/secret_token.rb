5f865034341904753728738cfd1e13857b5cc033
littleyang <littleyangqh@gmail.com>
2012-08-10 16:09:21 +0800

    bbs,rails research

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbs::Application.config.secret_token = '2d99aedef04b318d5ab4ee0dc898157381158a3d5edbc2dcc8ade6117054949f83189b8335c5007fa915f4eb7947918d75365cacf94de17b58688a20306bca42'
