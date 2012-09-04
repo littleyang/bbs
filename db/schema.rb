74e9f10b3637a85eadea9d1b118265f48046ff6a
littleyang <littleyangqh@gmail.com>
2012-09-04 10:27:48 +0800

    add admin controller/dashboard/backround platform

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,38 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120811151907) do
+ActiveRecord::Schema.define(:version => 20120904022004) do
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
 
   create_table "articles", :force => true do |t|
     t.string   "articleTitle",     :limit => 64
     t.string   "subTitle",         :limit => 128
-    t.string   "articleContent",   :limit => 1024
+    t.text     "articleContent"
     t.integer  "posterId"
     t.string   "postUserName",     :limit => 64
     t.integer  "replayUserId"
     t.string   "replayUserName",   :limit => 64
     t.integer  "articleBoardId"
     t.string   "articleBoardName", :limit => 64
-    t.string   "articleFile",      :limit => 64
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
   end
 
   create_table "board_articles", :force => true do |t|
@@ -47,13 +64,22 @@ ActiveRecord::Schema.define(:version => 20120811151907) do
   end
 
   create_table "notices", :force => true do |t|
-    t.string   "noticeTitle",     :limit => 64
-    t.string   "noticeContent",   :limit => 256
-    t.string   "noticeFile",      :limit => 64
-    t.string   "noticePublisher", :limit => 64
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "replay_articles", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -66,8 +92,8 @@ ActiveRecord::Schema.define(:version => 20120811151907) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "userName",   :limit => 20
-    t.string   "passWord",   :limit => 20
+    t.string   "userName",               :limit => 20
+    t.string   "passWord",               :limit => 20
     t.string   "email"
     t.string   "telephone"
     t.string   "qqNum"
@@ -75,11 +101,24 @@ ActiveRecord::Schema.define(:version => 20120811151907) do
     t.string   "signature"
     t.string   "birthday"
     t.string   "address"
-    t.integer  "score",                    :default => 0
-    t.integer  "level",                    :default => 0
+    t.integer  "score",                                :default => 0
+    t.integer  "level",                                :default => 0
     t.integer  "articleId"
-    t.datetime "created_at",                              :null => false
-    t.datetime "updated_at",                              :null => false
+    t.datetime "created_at",                                           :null => false
+    t.datetime "updated_at",                                           :null => false
+    t.string   "adminemail",                           :default => "", :null => false
+    t.string   "encrypted_password",                   :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                        :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
